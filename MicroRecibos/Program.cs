da8f1e72549da0f6f047a231204dc2431fdfd4f8
Gonzalo Pizarro <gpizarrob368@hotmail.com>
2022-10-23 19:01:19 -0400

    Se adiciona direccion del backend

--- MicroRecibos/Program.cs
+++ MicroRecibos/Program.cs
@@ -4,13 +4,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-	BaseAddress = new Uri("https://localhost:44353/")
+	//BaseAddress = new Uri("https://localhost:44353/")
+	BaseAddress = new Uri("https://recibosmicro.azurewebsites.net")	
 });
 
 
